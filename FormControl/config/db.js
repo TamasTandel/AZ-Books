018715a05f51d731510cf22bbce5e2efce2fd23b
TamasTandel <168076926+TamasTandel@users.noreply.github.com>
2024-11-10 11:46:50 +0530

    Delete FormControl directory

--- FormControl/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const connection = mongoose.connect('mongodb://0.0.0.0/formcontrol').then(() => {
-    console.log('connected to data base')
-})
-
-module.exports = connection
